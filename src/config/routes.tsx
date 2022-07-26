b43cb4ce007701732e7baa412ec02cfbfdcfdd70
Eadweard <804875728@qq.com>
2022-07-26 23:28:24 +0800

    add

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,13 +1,23 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import { Foo } from '../views/Foo';
-import { Bar } from '../views/Bar';
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import { Foo } from "../views/Foo";
+import { Bar } from "../views/Bar";
 
-const routes = [
-    { path: '/', component: Foo },
-    { path: '/about', component: Bar }
-]
+const routes: RouteRecordRaw[] = [
+  { path: "/", component: Foo },
+  { path: "/about", component: Bar },
+  {
+    path: "/welcome",
+    component: Foo,
+    children: [
+      { path: "/1", component: Foo },
+      { path: "/2", component: Foo },
+      { path: "/3", component: Foo },
+      { path: "/4", component: Foo },
+    ],
+  },
+];
 
 export const router = createRouter({
   history: createWebHashHistory(),
   routes,
-})
+});
---
+++ src/views/Welcome.tsx
@@ -0,0 +1,7 @@
+import { defineComponent } from "vue";
+
+export const Welcome = defineComponent({
+  setup() {
+    return () => <div></div>;
+  },
+});
