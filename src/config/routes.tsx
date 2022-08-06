d81843bd332cf0a46803a6ada9d024ebcb30ac54
Eadweard <804875728@qq.com>
2022-08-06 12:11:14 +0800

    fix

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -8,12 +8,13 @@ import { Foo } from "../views/Foo";
 import { Welcome } from "../views/Welcome";
 
 export const routes: RouteRecordRaw[] = [
-  { path: '/', component: Foo },
+  { path: '/', redirect: '/welcome' },
   { path: '/about', component: Bar },
   {
     path: '/welcome',
     component: Welcome,
     children: [
+      { path: '', redirect: '/welcome/1' },
       { path: '1', component: First, },
       { path: '2', component: Second, },
       { path: '3', component: Third, },
