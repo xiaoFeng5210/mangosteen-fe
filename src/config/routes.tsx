4558070e38515f19a2f5f9b73fefc56eb522421a
Eadweard <804875728@qq.com>
2022-08-06 11:19:56 +0800

    安装sass

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -14,10 +14,10 @@ const routes: RouteRecordRaw[] = [
     path: "/welcome",
     component: Welcome,
     children: [
-      { path: "/1", component: First },
-      { path: "/2", component: Second },
-      { path: "/3", component: Third },
-      { path: "/4", component: Fourth },
+      { path: "1", component: First },
+      { path: "2", component: Second },
+      { path: "3", component: Third },
+      { path: "4", component: Fourth },
     ],
   },
 ];
