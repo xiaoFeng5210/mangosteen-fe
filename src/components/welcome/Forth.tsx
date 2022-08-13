59a892d4fd515ccb9110fc8441e5736a907bb81d
Eadweard <804875728@qq.com>
2022-08-13 11:50:22 +0800

    组件更新

--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,8 +1,20 @@
-import { defineComponent } from 'vue';
-export const Forth = defineComponent({
-  setup: (props, context) => {
-    return () => (
-      <div>Forth</div>
-    )
-  }
-})
+import s from './WelcomeLayout.module.scss';
+import cloud from '../../assets/icons/cloud.svg';
+import { RouterLink } from 'vue-router';
+import { WelcomeLayout } from './WelcomeLayout';
+export const Forth = () => (
+  <WelcomeLayout>
+    {{
+      icon: () => <img class={s.icon} src={cloud} />,
+      title: () => <h2>每日提醒<br />不遗漏每一笔账单</h2>,
+      buttons: () => <>
+        <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+        <RouterLink to="/start" >完成</RouterLink>
+        <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+      </>
+
+    }}
+  </WelcomeLayout>
+)
+
+Forth.displayName = 'Forth'
