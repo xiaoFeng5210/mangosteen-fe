8882608b05fae73bb8e7976276b05da335ca9d5b
Eadweard <804875728@qq.com>
2022-08-27 20:53:34 +0800

    完善页面

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,4 +1,6 @@
 import { RouteRecordRaw } from "vue-router";
+import { ItemCreate } from "../components/item/ItemCreate";
+import { ItemList } from "../components/item/ItemList";
 import { First } from "../components/welcome/First";
 import { FirstActions } from "../components/welcome/FirstActions";
 import { Forth } from "../components/welcome/Forth";
@@ -7,6 +9,8 @@ import { Second } from "../components/welcome/Second";
 import { SecondActions } from "../components/welcome/SecondActions";
 import { Third } from "../components/welcome/Third";
 import { ThirdActions } from "../components/welcome/ThirdActions";
+import { ItemPage } from "../views/ItemPage";
+import { StartPage } from "../views/StartPage";
 import { Welcome } from "../views/Welcome";
 
 export const routes: RouteRecordRaw[] = [
@@ -15,11 +19,19 @@ export const routes: RouteRecordRaw[] = [
     path: '/welcome',
     component: Welcome,
     children: [
-      { path: '', redirect: '/welcome/1', },
-      { path: '1', components: { main: First, footer: FirstActions }, },
-      { path: '2', components: { main: Second, footer: SecondActions }, },
-      { path: '3', components: { main: Third, footer: ThirdActions }, },
-      { path: '4', components: { main: Forth, footer: ForthActions }, },
+      { path: '', redirect: '/welcome/1' },
+      { path: '1', name: "Welcome1", components: { main: First, footer: FirstActions }, },
+      { path: '2', name: "Welcome2", components: { main: Second, footer: SecondActions }, },
+      { path: '3', name: "Welcome3", components: { main: Third, footer: ThirdActions }, },
+      { path: '4', name: "Welcome4", components: { main: Forth, footer: ForthActions }, },
+    ]
+  },
+  { path: '/start', component: StartPage },
+  {
+    path: '/items', component: ItemPage,
+    children: [
+      { path: '', component: ItemList },
+      { path: 'create', component: ItemCreate },
     ]
   }
 ]
